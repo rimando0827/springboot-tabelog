5a3c63b3c125d17b0d97d81db9b0d3c9cf3fdb9b
rimando0827 <kasaharashojirou@gmail.com>
2025-03-31 22:49:35 +0900

    絞り込みおよび並び替え機能の追加

--- springboot-tabelog/src/main/java/com/example/springboot_tabelog/repository/CategoryRepository.java
+++ springboot-tabelog/src/main/java/com/example/springboot_tabelog/repository/CategoryRepository.java
@@ -2,10 +2,13 @@ package com.example.springboot_tabelog.repository;
 
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springboot_tabelog.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	public Page<Category> findBynameLike(String name, Pageable pageable );
 
 }
--- springboot-tabelog/src/main/java/com/example/springboot_tabelog/repository/ShopRepository.java
+++ springboot-tabelog/src/main/java/com/example/springboot_tabelog/repository/ShopRepository.java
@@ -8,5 +8,16 @@ import com.example.springboot_tabelog.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
 	public Page<Shop> findByNameLike(String keyword, Pageable pageable);
+	
+	public Page<Shop> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);  
+	public Page<Shop> findByNameLikeOrAddressLikeOrderByPriceLowerLimitAsc(String nameKeyword, String addressKeyword, Pageable pageable);
+	
+	public Page<Shop> findByCategoryIdOrderByCreatedAtDesc(Integer categoryId, Pageable pageable);
+	 Page<Shop> findByCategoryIdOrderByPriceLowerLimitAsc(Integer categoryId, Pageable pageable);
+	                  
+	
+	public Page<Shop> findAllByOrderByCreatedAtDesc(Pageable pageable);
+	public Page<Shop> findAllByOrderByPriceLowerLimitAsc(Pageable pageable);    
+	
 
 }
